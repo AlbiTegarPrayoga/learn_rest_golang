5c966be3047a2f96eeb4581b494d1e17f3c8eee7
AlbiTegarPrayoga <tegarp.albi@gmail.com>
2023-03-16 19:52:10 +0700

    learn rest golang

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"./config"
+	"./controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db := config.DBInit()
+	inDB := &controllers.InDB{DB: db}
+
+	router := gin.Default()
+
+	router.GET("/person/:id", inDB.GetPerson)
+	router.GET("/persons", inDB.GetPersons)
+	router.POST("/person", inDB.CreatePerson)
+	router.PUT("/person", inDB.DeletePerson)
+	router.DELETE("/person/:id", inDB.DeletePerson)
+	router.Run(":3000")
+}
